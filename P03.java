d3db220e6d493779e83df720c4cb38a8bd91a17f
Eloi <eloi@InstallParty.myguest.virtualbox.org>
2025-09-25 16:26:31 +0200

    PrimerJava.java

---
+++ P03.java
@@ -0,0 +1,17 @@
+//P03.java
+import java.util.*;
+public class P03{
+	public static void main (String[] args) {
+		String nom;
+		
+		Scanner scan = new Scanner (System.in);
+
+		System.out.println("Cóm et dius?");
+
+		nom = scan.nextLine();
+
+		System.out.println("El teu nom és: " + nom);
+	
+	}
+
+}
